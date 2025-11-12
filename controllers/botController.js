36d445f5052223cd1850537c71e899af376dcb4b
Hershey Marbeda <126095800+HersheyMarbeda@users.noreply.github.com>
2025-11-12 17:12:56 -0500

    Refactor location index retrieval and dean info response
    I didn't merge the codes corretly

--- controllers/botController.js
+++ controllers/botController.js
@@ -53,13 +53,8 @@ function getUnansweredQuestions() {
  */
 function getLocationIndexFromPrompt(prompt) {
     console.log(`${new Date().toISOString()} :: GETTING LOCATION`);
-    const index = locations.findIndex(location => location.title.toLowerCase() === prompt.toLowerCase());
-    console.log("Matched location index:", index);
-    if (index > -1) {
-        console.log("Matched location title:", locations[index].title);
-    }
-
-      options = {
+    
+    const options = {
         scorer: fuzz.token_set_ratio, // Any function that takes two values and returns a score, default: ratio
         processor: choice => choice.title,  // Takes choice object, returns string, default: no processor. Must supply if choices are not already strings.
         limit: 1, // Max number of top results to return, default: no limit / 0.
@@ -71,9 +66,11 @@ function getLocationIndexFromPrompt(prompt) {
     console.log(`${new Date().toISOString()} :: LOOKUP RESULTS: `);
     console.log(results);
 
-    if (results && results.length > 0 && results[0][1] >= 80 && results[0][2] != null) {
+    if (results && results.length > 0 && results[0][1] >= 80) {
+        console.log("Matched location index:", results[0][2]);
         return results[0][2];
     }
+    console.log("Matched location index:", -1);
     return -1;
 }
 
@@ -299,10 +296,10 @@ module.exports.query = (req, res) => {
                         matchedResponse = resp;
                         break;
                     }
-                    // Fuzzy match for greetings
-                    if (resp.intent === 'greetings') {
+                    // Fuzzy match for greetings and address_info
+                    if (resp.intent === 'greetings' || resp.intent === 'address_info') {
                         const fuzzScore = fuzz.token_set_ratio(prompt, p.toLowerCase());
-                        if (fuzzScore >= 80) {
+                        if (fuzzScore >= 70) {
                             matchedResponse = resp;
                             break;
                         }
@@ -339,7 +336,7 @@ module.exports.query = (req, res) => {
                 break;
             case 'phone_number_info':
                 if(locIndex > -1) {
-                    response = `<strong>${locations[locIndex].title} Contact Info:</strong><br><br>`;
+                    response = `<strong>${locations[locIndex].title} Contact Info:</strong><br>`;
                     response += `<i class='bx bxs-phone-call'></i>&nbsp;&nbsp;<a href='tel:${locations[locIndex].phone}'>${locations[locIndex].phone}</a><br>`;
                     response += `<i class='bx bxs-envelope' ></i>&nbsp;&nbsp;<a href="mailto:${locations[locIndex].email}">${locations[locIndex].email}</a>`;
                     response += `<br><br><a href='https://www.ivytech.edu/${locations[locIndex].url}' target='_blank'>Campus Page${suffix}`;
@@ -348,14 +345,12 @@ module.exports.query = (req, res) => {
                 }
                 break;
             case 'dean_info':
-                if(locIndex > -1 && locations[locIndex]?.dean) {
-                    response = `<strong>${locations[locIndex].dean.reply}</strong><br>`;
-                    response += `<br><br><a href='https://www.ivytech.edu/${locations[locIndex].dean.url}' target='_blank'>Campus Page${suffix}`;
-                } else if(locIndex > -1) {
-                    response = "Currently, there is no designated Dean for Information Technology at this campus. Please contact the campus administration for more information.";
-                }
-                else {
-                    response = "Which campus are you asking about?"
+                if(locIndex > -1) {
+                    response = `<strong>${matchedResponse.reply}</strong><br>`;
+                    response += `<br><br><a href='${buildWhitePagesURL('', '', locations[locIndex].title, 'faculty', 'Dean')}' target='_blank'>White Page${suffix}`;
+                } else {
+                    response = 'Hmm.. which campus are you wanting dean information for? You can also follow this link to search the White Pages for the dean: ';
+                    response += '<br><br><a href="' + buildWhitePagesURL('', '', '', 'faculty', 'Dean') + '" target="_blank">White Pages</a>';
                 }
                 break;    
             default:
@@ -381,7 +376,7 @@ module.exports.query = (req, res) => {
                 break;
             case 'phone_number_info':
                 if(locIndex > -1) {
-                    response = `<strong>${locations[locIndex].title} Contact Info:</strong><br><br>`;
+                    response = `<strong>${locations[locIndex].title} Contact Info:</strong><br>`;
                     response += `<i class='bx bxs-phone-call'></i>&nbsp;&nbsp;<a href='tel:${locations[locIndex].phone}'>${locations[locIndex].phone}</a><br>`;
                     response += `<i class='bx bxs-envelope' ></i>&nbsp;&nbsp;<a href="mailto:${locations[locIndex].email}">${locations[locIndex].email}</a>`;
                     response += `<br><br><a href='https://www.ivytech.edu/${locations[locIndex].url}' target='_blank'>Campus Page${suffix}`;
@@ -390,33 +385,13 @@ module.exports.query = (req, res) => {
                 }
                 break;
             case 'dean_info':
-                if(locIndex > -1 && locations[locIndex]?.dean) {
-                    let deanReply = locations[locIndex].dean.reply;
-                    let nameMatch = deanReply.match(/is ([^.]+)\./i);
-                    let deanName = nameMatch ? nameMatch[1].trim() : '';
-                    let deanEmailMatch = deanReply.match(/Email: ([^,\s]+)/i);
-                    let deanEmail = deanEmailMatch ? deanEmailMatch[1].trim() : '';
-                    let deanPhoneMatch = deanReply.match(/Phone: ([^,\s]+)/i);
-                    let deanPhone = deanPhoneMatch ? deanPhoneMatch[1].trim() : '';
-                    // Get main info (before Email:)
-                    let mainInfo = deanReply.split('Email:')[0].replace(/Phone:.*/, '').trim();
-                    // Bold dean name if found
-                    if (nameMatch && deanName) {
-                        mainInfo = mainInfo.replace(deanName, `<strong>${deanName}</strong>`);
-                    }
-                    response = mainInfo;
-                    if (deanEmail) {
-                        response += `<br><br>Email: <a href='mailto:${deanEmail}'>${deanEmail}</a>`;
-                    }
-                    if (deanPhone) {
-                        response += `<br>Phone: <a href='tel:${deanPhone}'>${deanPhone}</a>`;
-                    }
-                    response += `<br><br><a href='${locations[locIndex].dean.url}' target='_blank'>Dean's Page${suffix}`;
-                } else if(locIndex > -1) {
-                    response = "Currently, there is no designated Dean for Information Technology at this campus. Please contact the campus administration for more information.";
+                if(locIndex > -1) {
+                    response = `<strong>I can help you find information about the dean!</strong><br>`;
+                    response += `<br><br><a href='${buildWhitePagesURL('', '', locations[locIndex].title, 'faculty', 'Dean')}' target='_blank'>White Page${suffix}</a>`;
                 }
                 else {
-                    response = "Which campus are you asking about?"
+                    response = 'Hmm.. which campus are you wanting dean information for? You can also follow this link to search the White Pages for the dean: ';
+                    response += '<br><br><a href="' + buildWhitePagesURL('', '', '', 'faculty', 'Dean') + '" target="_blank">White Pages</a>';
                 }
                 break;    
             default:
